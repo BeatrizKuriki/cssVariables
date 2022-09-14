8f44a91734462e3719a082ff474c072fb400c8d5
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-14 00:20:48 -0300

    change: css and HTML

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 const inputs = document.querySelectorAll('.controls input');
 
-function handleUpdate(){
-    console.log(this.value);
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mouse', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
